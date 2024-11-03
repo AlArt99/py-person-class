86b148774474f9a1f91741f74f1e70a1534ee2ab
AlArt99 <AlArt1991@gmail.com>
2024-11-03 22:50:18 +0100

    updated with flake8 changes

--- app/main.py
+++ app/main.py
@@ -1,16 +1,13 @@
 class Person:
-
     people = {}
 
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-
         Person.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
-
     instances = []
 
     for person in people:
@@ -24,20 +21,19 @@ def create_person_list(people: list) -> list:
         name = person["name"]
 
         if "wife" in person or "husband" in person:
-
             spouse_key = "wife" if "wife" in person else "husband"
             spouse_name = person[spouse_key]
 
             if spouse_name is not None:
                 if spouse_name in Person.people:
-
                     person_instance = Person.people[name]
                     spouse_instance = Person.people[spouse_name]
 
                     setattr(person_instance, spouse_key, spouse_instance)
                 else:
-                    print(f"Warning: Spouse "
-                          f"\"{spouse_name}\" for "
-                          f"\"{name}\" not found.")
+                    print(
+                        f"Warning: Spouse \"{spouse_name}\" for \"{name}\" "
+                        "not found."
+                    )
 
     return instances
